a7e96e6db88a69aeb201c051eaea712bb4ca1131
Vidya-4 <83172212+Vidya-4@users.noreply.github.com>
2021-09-08 20:39:08 +0530

    Adding the GUI of login screen

--- components/SignIn.js
+++ components/SignIn.js
@@ -3,16 +3,12 @@ import {
     StyleSheet,
     Text,
     View,
-    TextInput,
-    Button,
     TouchableHighlight,
     Image,
     Alert,
     SafeAreaView
 } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { getUsersByEmail, addUsers } from '../services/Users';
-import SignUp from './SignUp';
+import { TextInput } from 'react-native-paper';
 
 
 export default class SignIn extends Component {
@@ -30,23 +26,13 @@ export default class SignIn extends Component {
         this.props.navigation.navigate('Home', {});
     }
 
-    goToSignup = () => {
-        this.props.navigation.navigate('SignUp', {});
-    }
-
-    re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 
     render() {
         return (
             <SafeAreaView style={styles.container}>
                 <View style={{flexDirection:'row'}}>
-                    <TouchableOpacity style={[ styles.loginButton1 ]}>
-                        <Text style={[styles.login1]}>SIGN IN</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[ styles.loginButton2 ]} onPress={this.goToSignup}>
-                        <Text style={[styles.login2]}>SIGN UP</Text>
-                    </TouchableOpacity>
+                        <Text style={[styles.title]}>EMPLOYEE APP</Text>
                 </View>
                 <View style={styles.inputContainer}>
                     <TextInput style={styles.inputs}
@@ -66,15 +52,12 @@ export default class SignIn extends Component {
                         onChangeText={(password) => this.setState({ password })} />
                 </View>
 
-                <View styles={styles.continue}>
-                <TouchableHighlight style={[styles.continueContainer, styles.loginButton]} onPress={() => this.onLoginButton()}>
-                    <Text style={styles.loginText}>CONTINUE</Text>
+                <View styles={styles.login}>
+                <TouchableHighlight style={[styles.loginContainer]} onPress={() => this.onLoginButton()}>
+                    <Text style={styles.loginText}>LOGIN</Text>
                 </TouchableHighlight>
                 </View>
 
-                <View>
-                    <Text style={styles.forgotPassword}>FORGOT PASSWORD</Text>
-                </View>
             </SafeAreaView>
         );
     }
@@ -86,31 +69,22 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'center',
     },
-    continue: {
+    login: {
         marginTop: 200,
     },
     inputContainer: {
-        width: 250,
+        width: 300,
         height: 45,
-        marginBottom: 20,
+        margin: 15,
         flexDirection: 'row',
         alignItems: 'center'
     },
     inputs: {
         marginTop: 29,
         height: 45,
-        borderBottomColor: 'grey',
-        borderBottomWidth: 1,
         flex: 1,
         color: 'black',
         fontSize: 20,
-    },
-    continueContainer: {
-        height: 50,
-        width: 250,
-        borderRadius: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
     inputIcon: {
         width: 30,
@@ -118,40 +92,22 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         justifyContent: 'center'
     },
-    continueContainer: {
+    loginContainer: {
         marginTop: 50,
         height: 50,
-        width: 300,
+        width: 250,
         borderRadius: 50,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: "#6200EE",
     },
-    loginButton2: {
-        borderRadius: 60,
-        padding: 15,
-        paddingLeft: 50,
-        paddingRight: 20,
-        marginRight: 20,
-    },
-    loginButton1: {
-        backgroundColor: "#6200EE",
-        borderRadius: 60,
-        padding: 15,
-        paddingLeft: 20,
-        paddingRight: 20,
-        marginLeft: 20,
-        marginBottom:30
-    },
-    login2: {
-        color: 'grey',
-        fontSize: 16,
-        fontWeight: '600',
-    },
-    login1: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: '600',
+    title: {
+        padding: 5,
+        marginBottom:20,
+        marginTop: 150,
+        color: '#36454F',
+        fontSize: 27,
+        fontWeight: 'bold',
     },
     loginText: {
         color: 'white',
