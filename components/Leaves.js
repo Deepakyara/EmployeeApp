d49695dbaaf6e3fa54a7437153a953db72393644
Preetham134 <63408868+Preetham134@users.noreply.github.com>
2021-09-10 18:12:24 +0530

    Update Leaves.js

--- components/Leaves.js
+++ components/Leaves.js
@@ -1,80 +1,92 @@
 import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Button,
-    TouchableHighlight,
-    Image,
-    Alert,
-    SafeAreaView
-} from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { getleaves } from '../services/LeavesData';
-//import { LeaveRequest } from './components/LeaveRequest';
+  import {
+      StyleSheet,
+      Text,
+      View,
+      TextInput,
+      Button,
+      TouchableHighlight,
+      Image,
+      Alert,
+      SafeAreaView,
+      FlatList,
+      ScrollView,
+      TouchableOpacity
+  } from 'react-native';
+import { getLeavesById } from '../services/LeavesPSQL';
+import { useEffect,useState } from 'react';
 
 
-export default class Leave extends Component {
-    constructor(props) {
-        super(props);
-    }
+const Item = ({ item, onPress,style }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.x, style]}>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+      }}>
+        <View style={{flex:2,  height: 50}} >
+          <Text style={styles.body}> ID               : {item.id}</Text>
+          <Text style={styles.body}> Startdate : {item.startdate}</Text>
+          <Text style={styles.body}> Enddate   : {item.enddate}</Text>
+          <Text style={styles.body}> Count       : {item.count}</Text>
+        </View>
+      </View>
+  </TouchableOpacity>
+);
 
-    onLeaveRequest = () => {
-        //alert('Leave.js line 22');
-        this.props.navigation.navigate('LeaveRequest');
-        }
-    
-    render() {
-        return (
-            <SafeAreaView>
-        <TouchableHighlight style={[styles.buttonContainer1, styles.loginButton1,styles.x]} onPress={() => this.onLeaveRequest()}>
-                <Text style={styles.loginText}>Leave Request</Text>
-        </TouchableHighlight>
-        <Text style={styles.base1Text}>Leave Details</Text>
-            </SafeAreaView>
-        );
-    }
-}
+export default function Leaves()  {
+  const [leave, setLeave] = useState(null);
+  const [count, doRender] = useState(0);
+  const [employees, setEmployees] = useState([]);
+
+
+  let loadEmployees = async () => {
+    let list = await getLeavesById(2);
+    setEmployees(list);
+  }
+
+  useEffect(()=>{
+      loadEmployees();
+  }, []);
+
+
+  const renderItem = ({ item }) => {
+    return (
+      <Item
+        item={item}
+        onPress={() => setLeave(item.id)}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList style={{margin:-90}}
+        data={employees}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={leave}
+      />
+      </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    base1Text:{
-        marginTop:50,
-        fontSize:24,
-        marginHorizontal:120,
-        fontWeight:"bold",
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#6200EE',
-        marginBottom:0,
-    },
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#ffffff',
-    },
-    buttonContainer1: {
-        height: 70,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 275,
-        borderRadius: 0,
-    },
-    loginButton1: {
-        backgroundColor: "#6200EE",
-        justifyContent: 'center',
-    },
-    x: {
-        marginTop:40,
-        justifyContent: 'center',
-        marginLeft:60,   
-    },
-    loginText: {
-        color: 'white',
-        fontWeight:"bold",
-        fontSize: 26,
-    },
-
+  body: {
+    color: '#000000',
+    fontSize: 20,
+    marginLeft:0,
+  },
+  x: {
+    padding: 90,
+    marginVertical: 0,
+    marginHorizontal: 0,
+    marginLeft:0,
+  },
+  container: {
+    backgroundColor:'white',
+    flex: 1,
+    marginTop: 0,
+},
 });
