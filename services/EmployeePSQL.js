11912c8aa57ab689a96b1847cf704f9b953d8bf2
Preetham134 <63408868+Preetham134@users.noreply.github.com>
2021-09-11 11:23:31 +0530

    ADD EmployeePSQL

---
+++ services/EmployeePSQL.js
@@ -0,0 +1,33 @@
+//var apiEndPoint = "https://nodeapi.pyther.com/Student";
+var apiEndPoint = "http://localhost:4000/api/employee";
+// GET https://nodeapi.pyther.com/Student fetch RECORDS
+// POST https://nodeapi.pyther.com/Student ADD RECORDS
+// PUT https://nodeapi.pyther.com/Student UPDATE RECORD
+// DELETE https://nodeapi.pyther.com/Student DELETE RECORD
+
+export var getEmployees = () =>{
+    return fetch(apiEndPoint, {
+        method: 'get',headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+    }).then(response => response.json()).then(response => {
+        //console.log(JSON.stringify(response));
+        return response;
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
+
+export var getEmployeeById = (id) =>{
+    return fetch(apiEndPoint+"/"+id, {
+        method: 'get',headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+    }).then(response => response.json()).then(response => {
+        console.log("hello 1")
+        console.log(JSON.stringify(response));
+        return response;
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
