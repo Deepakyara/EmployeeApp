51c94145dc4b739541ab72d690f639158a607a83
Vidya-4 <83172212+Vidya-4@users.noreply.github.com>
2021-09-12 18:00:30 +0530

    Update Leaves.js

--- components/Leaves.js
+++ components/Leaves.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Image,FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
 import { getLeavesById } from "../services/LeavesPSQL";
+import { getId, getToken } from "../services/Users";
 
 const Item = ({ item, onPress, style }) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
@@ -12,42 +12,36 @@ const Item = ({ item, onPress, style }) => (
         alignItems: 'stretch',
       }}>
       <View style={{flex:2,  height: 80}} >
-        <Text style={styles.phone}>Emp ID    : {item.employeeid}</Text>
-          <Text style={styles.phone}>Startdate   : {item.startdate}</Text>
-          <Text style={styles.phone}>Enddate    : {item.enddate}</Text>
-          <Text style={styles.phone}>Count       : {item.count}</Text>
+        <Text style={styles.details}>Emp ID    : {item.employeeid}</Text>
+          <Text style={styles.details}>Startdate   : {item.startdate}</Text>
+          <Text style={styles.details}>Enddate    : {item.enddate}</Text>
+          <Text style={styles.details}>Count       : {item.count}</Text>
         </View>  
       </View>
   </TouchableOpacity>
 );
 
-const CustomerApp = () => {
+const Leaves = () => {
   const [selectedId, setSelectedId] = useState(null);
-  const [count, doRender] = useState(0);
-  const [customers, setCustomers] = useState([]);
-  let navigation = useNavigation();  // #6e3b6e #f9c2ff  
+  const [leaves, setLeaves] = useState([]); 
 
-   loadCustomer = async()=>{
-    let list = await getLeavesById(1);
-    setCustomers(list);
-    console.log("list",list);
+
+    let id = getId();
+    let token = getToken();
+
+   loadLeaves = async()=>{
+    let list = await getLeavesById(id, token);
+    setLeaves(list);
   }
 
   useEffect(()=>{
-    const unsubscribe = navigation.addListener('focus', () => {
-      loadCustomer();
-    });
-    return unsubscribe;
-  },[navigation]);
-
-  DATA = customers;
-  console.log("DATA>>>>>>",DATA);
-
+    loadLeaves();
+  },[]);
 
 
   const renderItem = ({ item }) => {
     const backgroundColor = item.id === selectedId ? "#4682b4" : "#F7F6F2";
-
+    
     return (
       <Item
         item={item}
@@ -59,15 +53,12 @@ const CustomerApp = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      
       <FlatList
-        data={DATA}
+        data={leaves}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
         extraData={selectedId}
       />
-     
-
     </SafeAreaView>
   );
 };
@@ -85,41 +76,17 @@ const styles = StyleSheet.create({
     borderWidth:3,
     marginHorizontal: 16,
     shadowColor: "#07000e",
-
-shadowOpacity: 0.6,
-
-shadowRadius: 4,
-
-shadowOffset: {
-
-height: 6,
-
-width: 6
-
-}
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+    shadowOffset: {
+      height: 6,
+      width: 6
+    }
   },
-  title: {
-    fontSize: 32,
-    fontWeight:'bold',
-    color:'brown'
-  },
-  email: {
-    fontSize: 20,
-  },
-  phone: {
+  details: {
     fontSize: 19,
     fontFamily:"Times New Roman"
   },
-  address: {
-    fontSize: 18,
-  },
-  dob: {
-    fontSize: 16,
-  },
-  tinyLogo: {
-    width: 30,
-    height: 30,
-  },
 });
 
-export default CustomerApp;
+export default Leaves;
