2343f8d4fb04d1dad131023e1d66a1fd0081b1ef
Vidya-4 <83172212+Vidya-4@users.noreply.github.com>
2021-09-12 18:11:19 +0530

    Update Attendance.js

--- components/Attendance.js
+++ components/Attendance.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Image,FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
-import { useNavigation } from '@react-navigation/native';
-import { getAttendanceById } from "../services/AttendancePSQL";
-
-//let DATA = getCustomers();
-//console.log(">>>DATA>>>>>>>",DATA);
-
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
+import { getId, getToken } from "../services/Users";
+import { getAttendanceById } from '../services/AttendancePSQL';
 
 const Item = ({ item, onPress, style }) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
@@ -15,48 +11,37 @@ const Item = ({ item, onPress, style }) => (
         justifyContent: 'center',
         alignItems: 'stretch',
       }}>
-        {/* <View style={{flex:8, height: 50, flexDirection: 'row'}} >
-            <View style={{flex:7, height: 50}}>
-                <Text style={styles.title}>{item.id}</Text>
-            </View>
-        </View> */}
-        <View style={{flex:2,  height: 90}} >
-            <Text style={styles.phone}>DATE                  : {item.date.substring(0,10)}</Text>
-            <Text style={styles.phone}>IN TIME              : {item.intimedate.substring(11,19)}</Text>
-            <Text style={styles.phone}>OUT TIME          : {item.outtime.substring(11,19)}</Text>
-            <Text style={styles.phone}>TOTAL HOURS  : {item.totalhours}</Text>
+       <View style={{flex:2,  height: 90}} >
+            <Text style={styles.details}>DATE                  : {item.date.substring(0,10)}</Text>
+            <Text style={styles.details}>IN TIME              : {item.intimedate.substring(11,19)}</Text>
+            <Text style={styles.details}>OUT TIME          : {item.outtime.substring(11,19)}</Text>
+            <Text style={styles.details}>TOTAL HOURS  : {item.totalhours}</Text>
         </View>  
       </View>
   </TouchableOpacity>
 );
 
-const CustomerApp = () => {
+const Attendance = () => {
   const [selectedId, setSelectedId] = useState(null);
-  const [count, doRender] = useState(0);
-  const [customers, setCustomers] = useState([]);
-  let navigation = useNavigation();  // #6e3b6e #f9c2ff  
+  const [leaves, setLeaves] = useState([]); 
 
-   loadCustomer = async()=>{
-    let list = await getAttendanceById(1);
-    setCustomers(list);
-    console.log("list",list);
+
+    let id = getId();
+    let token = getToken();
+
+   loadLeaves = async()=>{
+    let list = await getAttendanceById(id, token);
+    setLeaves(list);
   }
 
   useEffect(()=>{
-    const unsubscribe = navigation.addListener('focus', () => {
-      loadCustomer();
-    });
-    return unsubscribe;
-  },[navigation]);
-
-  DATA = customers;
-  console.log("DATA>>>>>>",DATA);
-
+    loadLeaves();
+  },[]);
 
 
   const renderItem = ({ item }) => {
     const backgroundColor = item.id === selectedId ? "#4682b4" : "#F7F6F2";
-
+    
     return (
       <Item
         item={item}
@@ -68,15 +53,12 @@ const CustomerApp = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      
       <FlatList
-        data={DATA}
+        data={leaves}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
         extraData={selectedId}
       />
-     
-
     </SafeAreaView>
   );
 };
@@ -94,41 +76,17 @@ const styles = StyleSheet.create({
     borderWidth:3,
     marginHorizontal: 16,
     shadowColor: "#07000e",
-
-shadowOpacity: 0.6,
-
-shadowRadius: 4,
-
-shadowOffset: {
-
-height: 6,
-
-width: 6
-
-}
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+    shadowOffset: {
+      height: 6,
+      width: 6
+    }
   },
-  title: {
-    fontSize: 32,
-    fontWeight:'bold',
-    color:'brown'
-  },
-  email: {
-    fontSize: 20,
-  },
-  phone: {
+  details: {
     fontSize: 19,
     fontFamily:"Times New Roman"
   },
-  address: {
-    fontSize: 18,
-  },
-  dob: {
-    fontSize: 16,
-  },
-  tinyLogo: {
-    width: 30,
-    height: 30,
-  },
 });
 
-export default CustomerApp;
+export default Attendance;
